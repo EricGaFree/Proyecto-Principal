6b3b40589e9baef9350d02b13bfadd80ceb7dc14
polSoriano <pol.soriano@freematica.com>
2021-10-07 17:41:20 +0200

    añadido get de /blogs y /blogs/:id con las funciones creadas en blogs.controller y archivo comentarios.txt eliminado

--- pruba/backend/routes/blogs.routes.js
+++ pruba/backend/routes/blogs.routes.js
@@ -1,15 +1,17 @@
 import { Router } from "express";
-import { getAllBlogs, getBlogByID } from "../controllers/blogs.controller";
+import { getAllBlogs, getBlogByID } from "../controllers/blogs.controller.js";
 var router = Router();
 
 // Obtiene los datos de todos los blogs
 router.get("/", function(req, res, next) {
-    res.send(blogs);
+    const data = getAllBlogs();
+    res.status(200).send(data);
 });
 
 // Obtiene los datos de un blog
 router.get("/:id", function(req, res, next) {
-    res.send(blogs[req.params.id]);
+    const data = getBlogByID(req.params.id);
+    res.send(data);
 });
 
 export default router;
