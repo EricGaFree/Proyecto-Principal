786c9d1fe1a1f9635109f29e9165ff8075e14744
Eric <eric.garcia@freematica.com>
2021-10-07 16:11:29 +0200

    Proyecto funcinando expres mostrando angular

---
+++ src/backend/routes/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('*', (req, res) => {
+  res.sendFile('C:\\Users\\Eric Garcia\\Desktop\\Github\\Proyecto-Principal\\prueba_angular\\kappagod\\src\\index.html');
+});
+
+module.exports = router;
