19ff5c04bcadf01e7324f8d8587a87af18ca5afa
polSoriano <pol.soriano@freematica.com>
2021-10-13 17:04:53 +0200

    front end alberto

---
+++ pruba/backend/controllers/user.controller.js
@@ -0,0 +1,44 @@
+let users = {
+    "1": {
+        nombre: "Alberto",
+        apellido: "Matínez",
+        email: "alberto.martinez@freematica.com"
+    },
+    "2": {
+        nombre: "Eric",
+        apellido: "Garcia",
+        email: "eric.garcia@freematica.com"
+    },
+    "3": {
+        nombre: "Pol",
+        apellido: "Soriano",
+        email: "pol.soriano@freematica.com"
+    },
+    "4": {
+        nombre: "Carlos",
+        apellido: "Morales",
+        email: "carlos.morales@freematica.com"
+    }
+};
+/* GET home page. */
+
+export function getAllUsers() {
+    return users;
+}
+
+export function getUserByID(id) {
+    const data = users[id] ? users[id] : {msg: "Not found"};
+    return data;
+}
+
+export function createUser(req, res) {
+    return req.body;
+}
+
+export function deleteUsers() {
+    return { msg: "Usuarios eliminados" };
+}
+
+export function UpdateUser() {
+    return { msg: "User updated." };
+}
